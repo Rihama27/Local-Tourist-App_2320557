a0b394eb349cef8853f3af34f70841ed300c2a80
Rihama27 <azharihama@gmail.com>
2026-04-26 01:36:41 +0530

    Added footer.php file

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <p>© 2026 Local Tourist Day-Visit Planner</p>
+
+    <p>Developed by <span style="color:orange;">Rihama MAA</span></p>
+
+    <div class="footer-icons">
+        <a href="index.php"><i class="fas fa-home"></i></a>
+        <a href="#"><i class="fab fa-facebook"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fas fa-envelope"></i></a>
+    </div>
+</footer>
